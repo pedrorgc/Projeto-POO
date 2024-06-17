c6463e8ac200076507fd9d4554e6ad10c5d83e37
Luiz <luiz.felipe.reys.2005@gmail.com>
2024-06-17 15:48:13 -0300

    Last Update

--- pages/script/manager.js
+++ pages/script/manager.js
@@ -5,30 +5,30 @@ class Game {
         this.quantidade = quantidade;
         this.valor = valor;
     }
-  }
-  
-  document.addEventListener('DOMContentLoaded', function() {
+}
+
+document.addEventListener('DOMContentLoaded', function() {
     loadProducts();
-  
+
     document.getElementById('registerProdForm').addEventListener('submit', function(event) {
         event.preventDefault();
         saveProduct();
     });
-  });
-  
-  const modal = document.querySelector('.modal-container');
-  let products = JSON.parse(localStorage.getItem('products')) || [];
-  let currentProductIndex = null;
-  
-  function openModal(edit = false, index = 0) {
+});
+
+const modal = document.querySelector('.modal-container');
+let products = JSON.parse(localStorage.getItem('products')) || [];
+let currentProductIndex = null;
+
+function openModal(edit = false, index = 0) {
     modal.classList.add('active');
-  
+
     modal.onclick = e => {
         if (e.target.className.indexOf('modal-container') !== -1) {
             closeModal();
         }
     };
-  
+
     if (edit) {
         const product = products[index];
         document.getElementById('produto').value = product.produto;
@@ -40,37 +40,47 @@ class Game {
         document.getElementById('registerProdForm').reset();
         currentProductIndex = null;
     }
-  }
-  
-  function closeModal() {
+}
+
+function closeModal() {
     modal.classList.remove('active');
     document.getElementById('registerProdForm').reset();
     currentProductIndex = null;
-  }
-  
-  function saveProduct() {
+}
+
+function saveProduct() {
     const produto = document.getElementById('produto').value;
     const plataforma = document.getElementById('plataforma').value;
-    const quantidade = document.getElementById('quantidade').value;
+    const quantidade = parseInt(document.getElementById('quantidade').value, 10);
     const valor = document.getElementById('valor').value;
-  
+
     const newGame = new Game(produto, plataforma, quantidade, valor);
-  
-    if (currentProductIndex !== null) {
+
+    if (currentProductIndex !== null){
         products[currentProductIndex] = newGame;
+        console.log(`Produto atualizado: ${JSON.stringify(newGame)}`);
     } else {
+    const existingProductIndex = products.findIndex(product => 
+        product.produto === produto && product.plataforma === plataforma && product.valor === valor);
+
+        if (existingProductIndex !== -1) {
+        products[existingProductIndex].quantidade = 
+            parseInt(products[existingProductIndex].quantidade, 10) + quantidade;
+        console.log(`Quantidade atualizada: ${products[existingProductIndex].quantidade}`);
+        } else {
         products.push(newGame);
+        }
     }
-  
+    
     localStorage.setItem('products', JSON.stringify(products));
     loadProducts();
     closeModal();
-  }
-  
-  function loadProducts() {
+}
+
+function loadProducts() {
     const productsList = document.getElementById('productsList');
     productsList.innerHTML = '';
-  
+
     if (products.length === 0) {
         const listItem = document.createElement('tr');
         listItem.innerHTML = '<td colspan="6">Nenhum Produto Cadastrado.</td>';
@@ -93,11 +103,10 @@ class Game {
             productsList.appendChild(listItem);
         });
     }
-  }
-  
-  function deleteProduct(index) {
+}
+
+function deleteProduct(index) {
     products.splice(index, 1);
     localStorage.setItem('products', JSON.stringify(products));
     loadProducts();
-  }
-  
+}
